3441857b52c6c6655af4ea90d4a8c2812a325877
Silvia Pfeiffer <silviapfeiffer1@gmail.com>
2016-10-03 14:33:15 +1100

    Update composer autoload.

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -4,7 +4,7 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInit507c992c94bbd6317ffea5759ad707f4
+class ComposerStaticInit0ef401dc694b2c42e588afe05428ba89
 {
     public static $prefixLengthsPsr4 = array (
         'c' => 
@@ -33,9 +33,9 @@ class ComposerStaticInit507c992c94bbd6317ffea5759ad707f4
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInit507c992c94bbd6317ffea5759ad707f4::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInit507c992c94bbd6317ffea5759ad707f4::$prefixDirsPsr4;
-            $loader->prefixesPsr0 = ComposerStaticInit507c992c94bbd6317ffea5759ad707f4::$prefixesPsr0;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit0ef401dc694b2c42e588afe05428ba89::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit0ef401dc694b2c42e588afe05428ba89::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInit0ef401dc694b2c42e588afe05428ba89::$prefixesPsr0;
 
         }, null, ClassLoader::class);
     }
